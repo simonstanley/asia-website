d56d2d05f8c3a7b3559553f3d34545fe45616c96
Simon Stanley <simon.stanley87@gmail.com>
2025-06-29 18:04:21 +0100

    Add image links

--- js/lightbox.js
+++ js/lightbox.js
@@ -24,17 +24,13 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var captionText = document.getElementsByClassName("caption-container");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    captionText[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  captionText[slideIndex-1].style.display = "block";
 }
